93db65e07ec064011858e4a839559a0524354a97
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2024-01-16 23:14:47 +0700

    Add comment

--- app/src/main/java/com/example/android_service/MainActivity.java
+++ app/src/main/java/com/example/android_service/MainActivity.java
@@ -8,8 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+/**
+ https://www.youtube.com/watch?v=Zcq5wk6FPU0&t=187s
+ // video 3: Service : TinCoder
+ Start a Foreground Service trong Android (với Notification Channels) - [Service Part 3]
+
+ */
 public class MainActivity extends AppCompatActivity {
 
+// Khai báo view ở bên ngoài các vòng đời
     private EditText edt_data_intent;
     private Button btn_start_service;
     private Button btn_stop_service;
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Ánh xạ view ở bên trong vòng đời onCreate
         edt_data_intent = findViewById(R.id.edt_data_intent);
         btn_start_service= findViewById(R.id.btn_start_service);
         btn_stop_service = findViewById(R.id.btn_stop_service);
@@ -31,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 clickStartService();
             }
         });
+
         btn_stop_service.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,12 +48,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Hàm start Service
     private void clickStartService() {
+        // Put dữ liệu thông qua Intent này luôn
         Intent intent = new Intent(this, MyService.class);
-        intent.putExtra("key_data_intent", edt_data_intent.getText().toString().trim());
+        intent.putExtra("key_data_intent", edt_data_intent.getText().toString().trim());  // Đưa dữ liệu vào với cái key là key_data_intent
         startService(intent);
     }
 
+    // Hàm Stop Service
     private void clickStopService() {
         Intent intent = new Intent(this, MyService.class);
         stopService(intent);
--- app/src/main/java/com/example/android_service/MyService.java
+++ app/src/main/java/com/example/android_service/MyService.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+// extends từ Service trong android : bắt buộc phải overrider lại hàm onBind
 public class MyService extends Service {
     @Nullable
     @Override
@@ -19,28 +20,33 @@ public class MyService extends Service {
         return null;
     }
 
+
+    // Hàm quyết định service bị kill : và sau khi bộ nhớ có đủ thì service sẽ ứng sử sao
+    // Nơi nhận cái Intent mà ở StartService truyền tới
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
    String strDataIntent = intent.getStringExtra("key_data_intent");
-   sendNotification(strDataIntent);
+   sendNotification(strDataIntent);  // Chạy hàm gửi dữ liệu lên Notification
 
         return START_NOT_STICKY ;  // Không yêu cầu khởi động lại khi bị  kill đi
     }
 
+    // Hàm gửi dữ liệu string lên trên giao diện Notification
     private void sendNotification(String strDataIntent) {
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE |PendingIntent.FLAG_UPDATE_CURRENT);
         //  Để có thể mở được màn hình MainActivity khi bấm vào notification
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)   //  CHANNEL_ID được import ở bên MyApplication
-                .setContentTitle("Title notification service example")
-                .setContentText(strDataIntent)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-//                .setContentIntent(pendingIntent)
+                .setContentTitle("Title notification service example")  // Thêm title của notification
+                .setContentText(strDataIntent)  // Thêm content của notification
+                .setSmallIcon(R.drawable.ic_launcher_background)   // Thêm Icon của Notification
+                .setContentIntent(pendingIntent)   // Trường hợp nếu muốn ấn vào notification mà mở MainActivity lên
                 .build();
 
-        startForeground(1, notification);   //  Khởi động chạy chế độ foreground để hiện được notification
+        //  Khởi động chạy chế độ foreground để hiện được notification
+        startForeground(1, notification);
         // Để có thể hiển thị được notification
 
     }
